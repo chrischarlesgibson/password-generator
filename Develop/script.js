c9ef2082f4d0b0d9a6501aed49bdcc20d1943e41
Chris Gibson <chrischarlesgibson@gmail.com>
2022-09-27 22:59:12 -0500

    add button prompt and length alert

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,7 @@
+"use strict";
 // Assignment Code
+//declare var passwordLength
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,8 +10,18 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
+
+// Chris Gibson-add prompt that pops up when generate button is clicked. declare var PasswordLength and declare function generate password
+
+function generatePassword() {
+  var passwordLength;
+  passwordLength = prompt("What length do you want your password to be?");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters in length");
+  }
+}
